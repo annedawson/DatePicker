519666afe3c2cd088a0e4fc36307c5b092992010
annedawson <anne.dawson@gmail.com>
2024-12-30 15:51:11 -0800

    Third commit Mon 30 Dec 2024, 15:50 PT - Displays a date picker.

--- app/src/main/java/net/annedawson/datepicker/MainActivity.kt
+++ app/src/main/java/net/annedawson/datepicker/MainActivity.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import net.annedawson.datepicker.ui.theme.DatePickerTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.material3.Button
-import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,17 +45,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column (
+
+    var dateDialogController by remember { mutableStateOf(false) }
+    val dateState = rememberDatePickerState()
+
+    Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = { dateDialogController = true }) {
             Text(text = "Pick Date")
         }
-        Text(text = "Selected Date: ")
+
+        if (dateDialogController) {
+            DatePickerDialog(
+                onDismissRequest = { /*TODO*/ },
+                confirmButton = {
+                    TextButton(onClick = {
+                        dateDialogController = false
+                    }) {
+                        Text(text = "Ok")
+                    }
+                }
+            ) {
+                DatePicker(state = dateState)
+            }
+        }
+
+        Text(
+            text = "Selected Date: ",
+            modifier = Modifier.padding(top = 16.dp),
+            fontSize = 24.sp
+        )
+
     }
 }
 
